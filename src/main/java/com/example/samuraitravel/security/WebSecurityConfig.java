59ccebcd61c46713a8f61e19e088da1d67add338
VD3baeKy <digfehc@gmail.com>
2025-02-01 16:51:31 +0900

    Add files via upload

--- src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig {
          		 //.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**").permitAll()  // すべてのユーザーにアクセスを許可するURL   
        			//.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses").permitAll()  // すべてのユーザーにアクセスを許可するUR
        			//.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}").permitAll()  // すべてのユーザーにアクセスを許可するURL
-         		.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}", "/stripe/webhook").permitAll()  // すべてのユーザーにアクセスを許可するURL
+         		.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}", "/stripe/webhook","/error").permitAll()  // すべてのユーザーにアクセスを許可するURL
          		.requestMatchers("/houses/{houseId}/review/edit","/houses/{houseId}/review/delete").hasAnyRole("GENERAL", "ADMIN") // ROLE_GENERALまたはROLE_ADMINを持つユーザーにレビューの編集を許可
      			.requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
        			.anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
